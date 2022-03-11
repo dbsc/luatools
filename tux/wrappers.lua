87ae4297b2676991c0dc5c87d587d45e0841f609
André Dal Bosco <andredbsc@gmail.com>
2022-03-11 15:19:33 -0300

    refactor wrappers

--- tux/wrappers.lua
+++ tux/wrappers.lua
@@ -3,38 +3,55 @@ local M = {}
 local validators = require "tux.validators"
 
 
-function M.newcommand(name, body, nargs, default)
-    validators.validate_command_name(name)
-    nargs = nargs or 0
+local function texcmd(cmd, args, nopts, pos)
+    args = args or {}
+    nopts = nopts or 0
+    pos = pos or 1
 
-    local fmt, str
-    if default == nil then
-        fmt = [[\newcommand{\%s}[%d]{%s}]]
-        str = fmt:format(name, nargs, body)
-    else
-        fmt = [[\newcommand{\%s}[%d][%s]{%s}]]
-        str = fmt:format(name, nargs, default, body)
+    local str = string.format([[\%s]], cmd)
+    for i, arg in ipairs(args) do
+        if nopts > 0 and i >= pos then
+            str = str .. string.format("[%s]", arg)
+            nopts = nopts - 1
+        else
+            str = str .. string.format("{%s}", arg)
+        end
     end
 
     return str
 end
 
 
+function M.newcommand(name, body, nargs, default)
+    validators.validate_command_name(name)
+    nargs = nargs or 0
+
+    local args, nopts, pos
+    if default == nil then
+        args = {texcmd(name), nargs, body}
+        nopts = 1
+    else
+        args = {texcmd(name), nargs, default, body}
+        nopts = 2
+    end
+    pos = 2
+
+    return texcmd("newcommand", args, nopts, pos)
+end
+
+
 function M.directlua(str)
-    local fmt = [[\directlua{%s}]]
-    return fmt:format(str)
+    return texcmd("directlua", {str})
 end
 
 
 function M.luaescapestring(str)
-    local fmt = [[\luaescapestring{%s}]]
-    return fmt:format(str)
+    return texcmd("luaescapestring", {str})
 end
 
 
 function M.usepackage(str)
-    local fmt = [[\usepackage{%s}]]
-    return fmt:format(str)
+    return texcmd("usepackage", {str})
 end
 
 
